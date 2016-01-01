0a93eaa3ae03950a8a9f9b2a8ec46fbaf444f61d
Seth Creech <sacreech@moreheadstate.edu>
2016-01-01 01:38:43 -0500

    Initial commit (not close to completed yet)

---
+++ im_create.py
@@ -0,0 +1,22 @@
+from PIL import Image
+
+#width and height of the image
+width = 100
+height = 100
+
+img = Image.new(width, height)
+order = 2
+
+current_pixel = (0, 0)
+
+
+#sets a number of starting pixels to randomized RGBA values equal to the order of the Markov chain
+def orderedRandomPixels():
+    global img, order
+
+
+def setNextPixel():
+    global img, order
+    
+
+created_img = Image.new((100, 100));
